d4dd2fdf0018edf118b7cad6b1b07ee93f3d7e22
Pablo Morales <pablofmorales@gmail.com>
2012-08-21 00:05:13 -0300

    Add vhost config when we create a new project

--- Modules/Project.php
+++ Modules/Project.php
@@ -2,28 +2,44 @@
 
 class Project 
 {
+    private $_name;
+
     public function create($name)
     {
+        $this->_name = strtolower($name);
         render("Creating new project ... ");
-
-        $this->_createFolder($name);
-        $this->_bootstrap($name);
+        $this->_createFolder();
+        $this->_bootstrap();
+        $this->_vhost();
         
         render("Project created");
 
     }
 
-    private function _createFolder($name)
+    private function _vhost()
     {
-        if (file_exists($name)) {
-            render('The folder exists', 2);
-        } 
-        mkdir($name, 0755);
+        $vhost = file_get_contents(__DIR__ . '/../templates/vhost');
+    
+        $path = getcwd() . '/' . $this->_name; 
+        $vhost = str_replace("{path}", $path, $vhost);
+        $vhost = str_replace("{project}", $this->_name, $vhost);
+        
+        echo "\n Virtual Host, copy and paste the code below in your httpd.conf file\n";
+        echo "\n" . $vhost . "\n";
+        echo "\n\n####\n";
     }
 
-    private function _bootstrap($destiny)
+    private function _createFolder()
     {
-        exec('rsync -avz ' . __DIR__ . '/../templates/ZendSkeletonApplication/ --exclude .git* ' .  $destiny . '/' );
+        if (file_exists($this->_name)) {
+            render('The folder exists', 2);
+        } 
+        mkdir($this->_name, 0755);
+    }
+
+    private function _bootstrap()
+    {
+        exec('rsync -avz ' . __DIR__ . '/../templates/ZendSkeletonApplication/ --exclude .git* ' .  $this->_name . '/' );
     }
 }
 
